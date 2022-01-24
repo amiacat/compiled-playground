f500990980d92f520a4a226508b2345a4a4804dd
CatisHacker <cutec7782@gmail.com>
2022-01-24 10:45:36 +0000

    arrays.c

--- C/arrays.c
+++ C/arrays.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(){
-    int* numbers[1];
+    int* numbers[1] = {0};
     char response[2];
     int num;
 
@@ -13,17 +13,16 @@ int main(){
         printf("\nAdd a number to num...");
         
         scanf("%d", &num);
-        num = numbers[1];
+        
+        for(num = 0, num < 1, ++num){
+            num += numbers[num];
+        }
 
         printf("\nThe actual value of array is %d", numbers);
 
         printf("\nDo you want to proceed? ");
         scanf("%s", &response);
-
-        if(response != "Yes"){
-            break;
-        }
-    }
+    } // Dennis Ritchie, voce parece muito estressado
     
     return 0;
 }
